91041265914061f66844e0d1a95d04ce9811c747
Yaser Marey <yasser.marey@jrm.com.eg>
2023-01-22 02:51:13 +0200

    .

--- app/whatsapp_client.py
+++ app/whatsapp_client.py
@@ -33,36 +33,23 @@ class WhatsAppWrapper:
 
         response = requests.post(f"{self.API_URL}/messages", json=payload,headers=self.headers)
 
-        print(response.status_code)
-        print(response.text)
-        
         assert response.status_code == 200, "Error sending message"
 
         return response.status_code
 
-    def send(self, token, to, template_name, language_code):
-        url = 'https://graph.facebook.com/v15.0/111679144885464/messages'
-        headers = {
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json'
-        }
-        data = {
+
+    def send_text_message(self,to, message):
+        payload = {
             "messaging_product": 'whatsapp',
             "to": to,
-            "type": "template",
-            "template": {
-                "name": template_name,
-                "language": {
-                    "code": language_code
-                }
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": message
             }
         }
-        response = requests.post(url, json=data, headers=headers)
-        
-        print("\n" + str(response.json()))
-
+        response = requests.post(f"{self.API_URL}/messages", json=payload,headers=self.headers)
         assert response.status_code == 200, "Error sending message"
-
         return response.status_code
 
 
@@ -70,20 +57,31 @@ class WhatsAppWrapper:
         """_summary_: Process webhook notification
         For the moment, this will return the type of notification
         """
-
-        response = []
-
-        for entry in data["entry"]:
-
+        entries = data["entry"]
+        for entry in entries:
             for change in entry["changes"]:
-                response.append(
-                    {
-                        "type": change["field"],
-                        "from": change["value"],
-                    }
-                )
+                value = change["value"]
+                if value:
+                    phone_number_id = value["metadata"]["phone_number_id"]
+                    if "messages" in value:
+                        for message in value["messages"]:
+                            if message["type"] == "text":
+                                from_ = message["from"]
+                                message_body = message["text"]["body"]
+                                reply_message = f"Ack from FastAPI-WtsApp Webhook: {message_body}"
+                                
+                                client.send_text_message(phone_number_id, reply_message)
+                                return {
+                                    "statusCode": 200,
+                                    "body": json.dumps("Done"),
+                                    "isBase64Encoded": False
+                                }
 
-        return response
+        return {
+            "statusCode": 403,
+            "body": json.dumps("Unsupported method"),
+            "isBase64Encoded": False
+        }
 
 
 if __name__ == "__main__":
