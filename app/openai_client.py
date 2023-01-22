eacd8d21ab240d0c243719ccbade454908df5e74
Yaser Marey <yasser.marey@jrm.com.eg>
2023-01-22 16:15:17 +0200

    .

--- app/openai_client.py
+++ app/openai_client.py
@@ -15,7 +15,7 @@ class OpenAIWrapper:
     def complete(self, prompt):
         response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=prompt
+        prompt=prompt,
         temperature=0.7,
         max_tokens=256,
         top_p=1,
