8f71dcc78ea46120ed38902bc17ec39fdc4dc693
Yaser Marey <yasser.marey@jrm.com.eg>
2023-01-22 03:01:08 +0200

    .

--- app/whatsapp_client.py
+++ app/whatsapp_client.py
@@ -49,6 +49,8 @@ class WhatsAppWrapper:
             }
         }
         response = requests.post(f"{self.API_URL}/messages", json=payload,headers=self.headers)
+        print(response.status_code)
+        print(response.text)
         assert response.status_code == 200, "Error sending message"
         return response.status_code
 
