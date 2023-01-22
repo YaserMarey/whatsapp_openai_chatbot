60d4fcbb0bfbd536fb5a9b3713b4cc10c9b4b1de
Yaser Marey <yasser.marey@jrm.com.eg>
2023-01-23 00:01:21 +0200

    .

--- app/main.py
+++ app/main.py
@@ -1,50 +1,20 @@
 import os
+from fastapi import FastAPI, Request, Response
+from fastapi.encoders import jsonable_encoder
 
-from flask import Flask, jsonify, request
 from app.whatsapp_client import WhatsAppWrapper
 from app.openai_client import OpenAIWrapper
 
-app = Flask(__name__)
+app = FastAPI()
 
-VERIFY_TOKEN = os.environ.get('WHATSAPP_HOOK_TOKEN')
-
+VERIFY_TOKEN = os.environ.get("WHATSAPP_HOOK_TOKEN")
 
 @app.route("/")
 def hello_world():
     return "Hello World!!"
 
-
-@app.route("/send_message/", methods=["POST"])
-def send_message():
-    """_summary_: Send a message with a template to a phone number"""
-
-    if "language_code" not in request.json:
-        return jsonify({"error": "Missing language_code"}), 400
-
-    if "phone_number" not in request.json:
-        return jsonify({"error": "Missing phone_number"}), 400
-
-    if "template_name" not in request.json:
-        return jsonify({"error": "Missing template_name"}), 400
-
-    client = WhatsAppWrapper()
-
-    response = client.send_template_message(
-        template_name=request.json["template_name"],
-        language_code=request.json["language_code"],
-        phone_number=request.json["phone_number"],
-    )
-
-    return jsonify(
-        {
-            "data": response,
-            "status": "success",
-        },
-    ), 200
-
-
 @app.route("/webhook/", methods=["POST", "GET"])
-def webhook_whatsapp():
+def webhook_whatsapp(request: Request):
     """__summary__: Get message from the webhook"""
 
     if request.method == "GET":
@@ -58,14 +28,10 @@ def webhook_whatsapp():
     if response["statusCode"] == 200:
         print ("\n1")
         if response["body"] and response["from_no"]:
-            print ("\n2")
             openai_client = OpenAIWrapper()
-            print ("\n3")
             reply = openai_client.complete(prompt=response["body"])
-            print ("\n4")
             print ("\nreply is "  + reply)
             wtsapp_client.send_text_message(to=response["from_no"], message=reply)
-            print ("\n5")
             print ("We replied with " + str(response))
 
-    return jsonify({"status": "success"}, 200)
+    return jsonable_encoder({"status": "success"}, 200)
